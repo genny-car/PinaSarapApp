05b70816c616e8c97a556d56e81ef2ed05ec7a22
mocpagsuyuin2020 <78241250+mocpagsuyuin2020@users.noreply.github.com>
2022-12-22 22:44:42 +0800

    tangal ng code sa signinactivity.java

--- app/src/main/java/com/app/appdevproject/SignInActivity.java
+++ app/src/main/java/com/app/appdevproject/SignInActivity.java
@@ -17,23 +17,6 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
 
-        MaterialButton signinbtn = (MaterialButton) findViewById(R.id.signinbtn);
-
-        //admin and admin
-
-        signinbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                    //correct
-                    Toast.makeText(MainActivity.this, "You are successfully signed in", Toast.LENGTH_SHORT).show();
-                } else
-                    //incorrect
-                    Toast.makeText(MainActivity.this, "Signing in failed", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
