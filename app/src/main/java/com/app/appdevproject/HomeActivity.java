1b12e576c350c723fdf78fd142b94c466e066045
mocpagsuyuin2020 <78241250+mocpagsuyuin2020@users.noreply.github.com>
2022-12-23 15:59:29 +0800

    animation draft

--- app/src/main/java/com/app/appdevproject/HomeActivity.java
+++ app/src/main/java/com/app/appdevproject/HomeActivity.java
@@ -5,22 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class HomeActivity extends AppCompatActivity {
 
-    ImageButton profimage;
+    LottieAnimationView lottieprof;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        profimage = (ImageButton) findViewById(R.id.profimage);
+        lottieprof = findViewById(R.id.lottieprof);
 
-        profimage.setOnClickListener(view -> {
-            startActivity(new Intent(HomeActivity.this,ProfileActivity .class));
+        lottieprof.setOnClickListener(view -> {
+            lottieprof.playAnimation();
+
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(HomeActivity.this,ProfileActivity .class));
+                }
+            }, 2000);
+
         });
     }
+
 }
