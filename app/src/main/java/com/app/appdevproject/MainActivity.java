dad4c7d3d7c95a98719261d2c9f2dca705d830f4
mocpagsuyuin2020 <78241250+mocpagsuyuin2020@users.noreply.github.com>
2022-12-23 11:34:02 +0800

    loading screen animation for main activity

--- app/src/main/java/com/app/appdevproject/MainActivity.java
+++ app/src/main/java/com/app/appdevproject/MainActivity.java
@@ -2,15 +2,39 @@ package com.app.appdevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView loadingtxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progressBar);
+        loadingtxt = findViewById(R.id.loadingtxt);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressAnimation();
+    }
+    public void progressAnimation(){
+        LoadingScreenAnimation anim = new LoadingScreenAnimation(this, progressBar, loadingtxt, 0f, 100f);
+        anim.setDuration(5000);
+        progressBar.setAnimation(anim);
     }
 }
 
