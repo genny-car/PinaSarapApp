00a36c4ef93a1418946d69fb61df9fb4ace1bdb1
Genevieve V. Carranza <gvcarranza2020@plm.edu.ph>
2022-12-27 18:37:14 +0800

    Navigation Bar (Re-upload w/ code)

--- app/src/main/java/com/app/appdevproject/Main.java
+++ app/src/main/java/com/app/appdevproject/Main.java
@@ -1,6 +1,9 @@
 package com.app.appdevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.ClipData;
 import android.os.Bundle;
@@ -16,8 +19,36 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
-        binding.
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment();
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.saved:
+                    replaceFragment(new SavedFragment());
+                    break;
+
+
+            }
+
+
+            return true;
         })
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
