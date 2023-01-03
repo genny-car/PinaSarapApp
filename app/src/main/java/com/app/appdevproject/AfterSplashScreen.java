3a43bfcbb1298843775f98ce29001d936ca39b07
Genevieve V. Carranza <gvcarranza2020@plm.edu.ph>
2023-01-03 16:33:30 +0800

    Updated the old pages and the newly added pages: First page after splash scree, forgot password, verification, new password, sign up, edit profile and change password.

---
+++ app/src/main/java/com/app/appdevproject/AfterSplashScreen.java
@@ -0,0 +1,13 @@
+package com.app.appdevproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AfterSplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_splash_screen);
+    }
+}
