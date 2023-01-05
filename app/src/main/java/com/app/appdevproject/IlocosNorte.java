f7082947546118e0dd2897808f47b4e09cdec4ef
Genevieve V. Carranza <gvcarranza2020@plm.edu.ph>
2023-01-05 09:04:51 +0800

    Regions of Luzon Page, Provinces of Luzon Page and Foods inside the province.

---
+++ app/src/main/java/com/app/appdevproject/IlocosNorte.java
@@ -0,0 +1,14 @@
+package com.app.appdevproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IlocosNorte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ilocos_norte);
+    }
+}
---
+++ app/src/main/java/com/app/appdevproject/RegionI.java
@@ -0,0 +1,14 @@
+package com.app.appdevproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegionI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_region_i);
+    }
+}
